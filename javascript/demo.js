460e59dd91195970fdf5d401e3a7c90caaa29826
shahid <shahidthaha2005@gmail.com>
2024-12-04 09:04:07 +0530

    working

---
+++ javascript/demo.js
@@ -0,0 +1,52 @@
+// var a=10
+// var a=20
+// alert(a)
+// let a=10
+// a=20
+// const a=10
+// a=20
+// console.log(a);
+// var let const
+
+
+
+// let a=10.20  
+// let a="hello"
+// let a="123"
+// let a=[1,2,3,['abc']]
+// let a={name:'shahid',age:22}
+// let a=null  for giving null values
+// let a=true
+// console.log(typeof a);for checking the types of variables
+
+
+// operators
+// arithametic
+// +,-,*,/,**,++,--,
+
+// console.log(10+5);
+// console.log(10-5);
+// console.log(10*5);
+// console.log(10/5);
+
+// a=10
+// // a++
+// // ++a
+// console.log(a);
+
+// assignment 
+
+// =,+=,-=,/=,**=
+
+// ==
+
+// console.log(10=="10");
+// console.log(10==="10");
+
+
+// console.log(10!="11");
+// console.log(10!=="10");
+
+
+// console.log(10==10 && 10==11);  and
+// console.log(10==10 || 10==11);  or
